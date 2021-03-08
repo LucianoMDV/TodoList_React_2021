9f6c8937f21a8b8674bf30453dffde8ea1eaa4ab
Luciano D'Agata <martinvilla481@gmail.com>
2021-03-08 12:37:52 -0300

    antes de comunicarme entre componentes y manejar estados

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./Todo";
 
-const todos = [
+const initialTodos = [
   {
+    id: 1,
     title: "Todo #1",
     description: "Des del Todo #1",
     completed: false,
   },
   {
+    id: 2,
     title: "Todo #2",
     description: "Des del Todo #2",
     completed: true,
@@ -15,12 +17,16 @@ const todos = [
 ];
 
 const TodoList = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  //   const todos = state[0];
+  //   const setTodos = state[1];
+
   return (
     <div>
       <h1> Soy TodoList </h1>
-      {
-          todos.map(todo => <Todo todo={todo}/>)
-      }
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
